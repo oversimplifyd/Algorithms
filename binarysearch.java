5896e7943b9170c5677459a262fbc293b96d574e
Personal Development <personaldevelopment@CG-MB2020003.local>
2020-03-28 18:12:19 +0100

    Binary Search

---
+++ binarysearch.java
@@ -0,0 +1,44 @@
+package algorithms;
+/**
+* Binary Search 
+* Worst Case 0(logn)
+* floorIndex, CeilingIndex borders the input size for conveneince. 
+ */
+
+class BinarySearch {
+
+     public static void main(String[] args) {
+
+         int[] items =  {1, 2, 3, 4, 8, 11, 13, 0, 9, 7};
+
+         int searchValue = 3;
+
+         System.out.println(bSearch(items, searchValue));
+     }
+
+     public static boolean bSearch(int[] items, int searchValue) {
+
+         int floorIndex = -1;
+         int ceilingIndex = items.length;
+
+         while ((floorIndex + 1) < ceilingIndex) {
+
+             int distance = ceilingIndex - floorIndex;
+             int halfDistance = distance / 2; //Splits problem in half at every iteration. Gives of Logn 
+
+             int guessIndex = halfDistance + floorIndex; // Removes borders created by floor and ceilindexes.
+
+             if (items[guessIndex] == searchValue) return true; 
+
+             if (items[guessIndex] > searchValue) {
+
+                 ceilingIndex = guessIndex;
+             } else {
+
+                 floorIndex = guessIndex;
+             }
+         }
+
+         return false; 
+     }
+ }
