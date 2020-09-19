2424804accdbf5e9118f1e9f39490a09c066bfd8
Personal Development <thrustinv@gmail.com>
2020-09-19 22:14:36 +0200

    Removed the sluggish lcs impl

---
+++ lcs.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class LCS {
+
+
+    public static int lcs(String text1, String text2) {
+
+    }
+    
+    public static void main(String[] args){
+        System.out.println(lcsBruteForce("cbda", "abdca"));
+    }
+}
